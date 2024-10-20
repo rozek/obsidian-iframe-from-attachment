49efa5341ce98e19574836e8b3ede47b90fecb41
Andreas Rozek <A.Rozek@gmx.de>
2024-10-20 11:16:47 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,134 +1,48 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, MarkdownPostProcessor, MarkdownPostProcessorContext } from 'obsidian'
 
-// Remember to rename these classes and interfaces!
+export default class IframeAttachmentPlugin extends Plugin {
+  async onload() {
+    this.registerMarkdownCodeBlockProcessor('iframe-from-attachment', this.iFrameProcessor.bind(this));
+  }
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+  async iFrameProcessor (Source:string, Element:HTMLElement, Context:MarkdownPostProcessorContext) {
+    const Match = /^(\S+)(?:\s+(\d+)(?:x(\d+))?)?$/.exec(Source.trim())
+    if (Match == null) {
+      console.error('invalid arguments for "iframe-from-attachment"',Source)
+      Element.createEl('p', { text:`Error: invalid arguments for "iframe-from-attachment" ("${Source}")` })
+      return
+    }
+    
+    const [_, Attachment, Width,Height] = Match  
+    try {
+      const absolutePath = await this.getAbsolutePath(Attachment,Context)
+      
+      const iFrame = Element.createEl('iframe')
+      iFrame.src = absolutePath
+      if (Width  != null) { iFrame.width  = Width }
+      if (Height != null) { iFrame.height = Height }
+    } catch (Signal:any) {
+      console.error('"iframe-from-attachment": processing failed with',Signal)
+      Element.createEl('p', { text:`"iframe-from-attachment": processing failed with ${Signal.message}` })
+    }
+  }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+  async getAbsolutePath (Attachment:string, Context:MarkdownPostProcessorContext):Promise<string> {
+    const { vault }   = this.app
+    const currentFile = Context.sourcePath
+    
+    let AttachmentFile = vault.getFileByPath(Attachment)
+    if (AttachmentFile == null) {
+      const currentFolder = currentFile.substring(0,currentFile.lastIndexOf('/'))
+      AttachmentFile = vault.getFileByPath(
+        `${currentFolder}/${Attachment}`
+      )
+    }
+  
+    if (AttachmentFile == null) {
+      throw new Error(`"iframe-from-attachment": Attachment not found ("${Attachment}")`)
+    }
+  
+    return vault.getResourcePath(AttachmentFile)
+  }  
 }
